35a05d16039354f398d781d7cb4d358e94f79577
Vladislav Agafonov <vlad_islav_7589@mail.ru>
2023-12-14 04:28:09 +0600

    connect postgres

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:env_flutter/env_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:postgres/postgres.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,24 +37,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
+  List<List<dynamic>> data = [];
+  Future<List<List<dynamic>>> fetchDataFromDB() async {
+    final connection = await Connection.open(
+      Endpoint(
+        host: dotenv.env['host']!,
+        database: dotenv.env['database']!,
+        username: dotenv.env['username']!,
+        password: dotenv.env['password']!,
+      ),
+      // The postgres server hosted locally doesn't have SSL by default. If you're
+      // accessing a postgres server over the Internet, the server should support
+      // SSL and you should swap out the mode with `SslMode.verifyFull`.
+      settings: ConnectionSettings(sslMode: SslMode.disable),
+    );
+    print('has connection!');
 
+    try {
+      final results = await connection.execute(Sql.named('SELECT * FROM Товары'));
+      print(results);
+      return results;
+    } catch (e) {
+      print('Error: $e');
+      return [];
+    } finally {
+      await connection.close();
+    }
+  }
+
+  @override
+
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('PostgreSQL в Flutter'),
       ),
-      body: const Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                data = await fetchDataFromDB();
+                setState(() {});
+              },
+              child: Text('Загрузить данные'),
+            ),
+            data.isEmpty
+                ? Text('Данные не загружены')
+                : SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columns: [
+                  DataColumn(label: Text('Код товара')),
+                  DataColumn(label: Text('Наименование')),
+                  DataColumn(label: Text('Количество')),
+                  DataColumn(label: Text('Тип товара')),
+                  DataColumn(label: Text('Цена')),
+                  DataColumn(label: Text('Код поставщика')),
+                ],
+                rows: data.map((row) {
+                  return DataRow(
+                    cells: row.map((cell) {
+                      return DataCell(Text('$cell'));
+                    }).toList(),
+                  );
+                }).toList(),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
 }
